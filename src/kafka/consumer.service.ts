9eba86c46508becf4b9348ddc7a9c37bd6daa77e
sungkihwan <singahu@naver.com>
2022-03-24 18:33:09 +0900

    test

--- src/kafka/consumer.service.ts
+++ src/kafka/consumer.service.ts
@@ -1,15 +1,25 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnApplicationShutdown } from "@nestjs/common";
 import { Consumer, ConsumerRunConfig, ConsumerSubscribeTopic, Kafka } from "kafkajs";
 
 @Injectable()
-export class ConsumerService {
+export class ConsumerService implements OnApplicationShutdown {
     private readonly kafka = new Kafka({
-        brokers: ['localhost:9092'],
+        brokers: ['127.0.0.1:9092'],
     });
 
     private readonly consumers: Consumer[] = [];
 
     async consume(topic: ConsumerSubscribeTopic, config: ConsumerRunConfig) {
-        
+        const consumer = this.kafka.consumer({ groupId: 'nest-kafka' });
+        await consumer.connect();
+        await consumer.subscribe(topic);
+        await consumer.run(config);
+        this.consumers.push(consumer);
+    }
+
+    async onApplicationShutdown() {
+        for (const consumer of this.consumers) {
+            await consumer.disconnect();
+        }
     }
 }
--- src/kafka/producer.service.ts
+++ src/kafka/producer.service.ts
@@ -4,7 +4,7 @@ import { Kafka, Producer, ProducerRecord } from "kafkajs";
 @Injectable()
 export class ProducerService implements OnModuleInit, OnApplicationShutdown {
     private readonly kafka = new Kafka({
-        brokers: ['localhost:9092'],
+        brokers: ['127.0.0.1:9092'],
     });
     private readonly producer: Producer = this.kafka.producer();
 
